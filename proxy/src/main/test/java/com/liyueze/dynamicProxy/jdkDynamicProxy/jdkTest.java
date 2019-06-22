fa3b3cc9768b0ed70fc461ca251554784014566d
liyzzz <mail@liyueze.com>
2019-06-22 19:22:27 +0800

    Update jdkTest.java
    save jdk proxy class

--- proxy/src/main/test/java/com/liyueze/dynamicProxy/jdkDynamicProxy/jdkTest.java
+++ proxy/src/main/test/java/com/liyueze/dynamicProxy/jdkDynamicProxy/jdkTest.java
@@ -4,6 +4,8 @@ import java.lang.reflect.InvocationHandler;
 
 public class jdkTest {
     public static void main(String[] args) {
+        //设置jdk动态代理生成类保存成文件（文件的位置在项目根目录下com.sun.proxy）
+        System.getProperties().put("sun.misc.ProxyGenerator.saveGeneratedFiles", "true");
         IDao userDao= new UserDao();
         InvocationHandler myInvocationHandler=new MyInvocationHandler();
         try {
